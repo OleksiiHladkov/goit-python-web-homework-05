e418ac133d5b5e724da11aa8b11af7e79d123236
Oleksii Hladkov <agladkoff17@gmail.com>
2023-11-03 20:02:09 +0200

    little fix

--- web_socket_chat/server.py
+++ web_socket_chat/server.py
@@ -21,34 +21,36 @@ class Logger:
         self.time = time
         self.data = data
 
-    async def logging_exchange_command(self, response: list|str, message: str):
+    async def logging_exchange_command(self, response: list | str, message: str) -> None:
         rfn = resource_filename("web_socket_chat", self.path)
         async with async_open(rfn, "a+") as afp:
             format = "%d/%m/%Y, %H:%M:%S"
-            await afp.write(f"{self.time.strftime(format)} - user: {self.user.name} - command: [{self.data}] - response: {str(response)} - message: [{message}]\n")
+            await afp.write(
+                f"{self.time.strftime(format)} - user: {self.user.name} - command: [{self.data}] - response: {str(response)} - message: [{message}]\n"
+            )
 
 
 class Server:
     clients = set()
 
-    async def register(self, ws: WebSocketServerProtocol):
+    async def register(self, ws: WebSocketServerProtocol) -> None:
         ws.name = names.get_full_name()
         self.clients.add(ws)
-        logging.info(f'{ws.remote_address} connects')
+        logging.info(f"{ws.remote_address} connects")
 
-    async def unregister(self, ws: WebSocketServerProtocol):
+    async def unregister(self, ws: WebSocketServerProtocol) -> None:
         self.clients.remove(ws)
-        logging.info(f'{ws.remote_address} disconnects')
+        logging.info(f"{ws.remote_address} disconnects")
 
-    async def send_to_client(self, message: str, ws: WebSocketServerProtocol):
+    async def send_to_client(self, message: str, ws: WebSocketServerProtocol) -> None:
         if self.clients:
             await ws.send(message)
-    
-    async def send_to_clients(self, message: str):
+
+    async def send_to_clients(self, message: str) -> None:
         if self.clients:
             [await client.send(message) for client in self.clients]
 
-    async def ws_handler(self, ws: WebSocketServerProtocol):
+    async def ws_handler(self, ws: WebSocketServerProtocol) -> None:
         await self.register(ws)
         try:
             await self.distrubute(ws)
@@ -57,32 +59,32 @@ class Server:
         finally:
             await self.unregister(ws)
 
-    async def distrubute(self, ws: WebSocketServerProtocol):
+    async def distrubute(self, ws: WebSocketServerProtocol) -> None:
         async for message in ws:
             split_message = message.strip().split()
-            
+
             if len(split_message) and split_message[0] == "exchange":
                 logger = Logger("exchange_log_file.txt", ws, datetime.now(), message)
-                
+
                 if len(split_message) == 1:
                     split_message.append("1")
                     exchange_data = await exchange_rates.main(split_message)
                 else:
                     exchange_data = await exchange_rates.main(message.strip().split())
-                
-                new_message = await self.exchange_data_handler(exchange_data)                
+
+                new_message = await self.exchange_data_handler(exchange_data)
                 await self.send_to_client(new_message, ws)
-                
+
                 await logger.logging_exchange_command(exchange_data, new_message)
             else:
                 await self.send_to_clients(f"{ws.name}: {message}")
 
-    async def exchange_data_handler(self, exchange_data: list[dict]|str):
+    async def exchange_data_handler(self, exchange_data: list[dict] | str) -> str:
         if isinstance(exchange_data, str):
             return exchange_data
-        
+
         result = ""
-        
+
         for date in exchange_data:
             for date_key, date_value in date.items():
                 result += date_key + ": "
@@ -96,8 +98,9 @@ class Server:
 
 async def main():
     server = Server()
-    async with websockets.serve(server.ws_handler, 'localhost', 8080):
+    async with websockets.serve(server.ws_handler, "localhost", 8080):
         await asyncio.Future()  # run forever
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
